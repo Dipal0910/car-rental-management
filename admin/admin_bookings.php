67796e0a51e8666646884196ad4f85aaed5a0993
Yuvraj <Yuvraj363singh@gmail.com>
2025-04-03 13:15:16 -0400

    Final Commit 2

--- admin/admin_bookings.php
+++ admin/admin_bookings.php
@@ -25,6 +25,27 @@ $result = $conn->query($sql);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4">
+  <a class="navbar-brand" href="#">CarRental</a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navContent">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navContent">
+    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+      <li class="nav-item"><a class="nav-link" href="../index.php">Home</a></li>
+      <li class="nav-item"><a class="nav-link" href="../about.php">About</a></li>
+      <li class="nav-item"><a class="nav-link" href="../contact.php">Contact</a></li>
+      <li class="nav-item"><a class="nav-link" href="../login.php">Login</a></li>
+      <li class="nav-item"><a class="nav-link" href="../signup.php">Sign Up</a></li>
+      <li class="nav-item">
+        <button class="btn btn-outline-light btn-sm ms-2" id="toggleTheme">🌗 Theme</button>
+      </li>
+    </ul>
+  </div>
+</nav>
+
 <body class="bg-light">
     <div class="container mt-5">
         <h2 class="text-center mb-4">Pending Bookings</h2>
